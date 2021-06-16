65006440ba1f25a6594911a7cb628d5b1bb6b9ab
fabian-bouche-liferay <fabian.bouche@liferay.com>
2021-06-16 18:17:16 +0200

    initial commit

---
+++ modules/redirect-filter/src/main/java/com/liferay/samples/fbo/redirect/filter/RedirectFilter.java
@@ -0,0 +1,105 @@
+package com.liferay.samples.fbo.redirect.filter;
+
+import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
+import com.liferay.portal.servlet.filters.BasePortalFilter;
+import com.liferay.samples.fbo.redirect.filter.configuration.RedirectFilterConfiguration;
+
+import java.io.IOException;
+import java.util.Map;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Modified;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Fabian-Liferay
+ */
+@Component(
+		immediate = true,
+		configurationPid = "com.liferay.samples.fbo.redirect.filter.configuration.RedirectFilterConfiguration",
+        property = {
+        		"dispatcher=FORWARD",
+        		"dispatcher=REQUEST",
+        		"servlet-context-name=",
+                "servlet-filter-name=New Site Redirect Filter",
+                "url-pattern=/*"
+        },		
+		service = Filter.class
+		)
+public class RedirectFilter extends BasePortalFilter {
+
+	private final static Logger LOG = LoggerFactory.getLogger(RedirectFilter.class);
+
+	@Override
+	public void processFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+
+		LOG.debug("Do RedirectFilter {}", request.getRequestURL());
+
+		LOG.debug("Is RedirectFilter enabled? {}", this._redirectFilterConfiguration.enabled());
+		if(this._redirectFilterConfiguration.enabled()) {
+
+			LOG.debug("Virtual host: {}", request.getServerName());
+			if(virtualHostIsTargetted(request)) {
+			
+				String cookieName = this._redirectFilterConfiguration.redirectTriggerCookieName();
+				if(hasCookie(cookieName, request)) {
+
+					LOG.debug("Redirecting user to: {}", this._redirectFilterConfiguration.redirectFilterUrl());
+
+					response.sendRedirect(this._redirectFilterConfiguration.redirectFilterUrl());
+					
+					return;
+				}
+				
+			}
+				
+		}
+		chain.doFilter(request, response);
+
+	}
+	
+	private boolean virtualHostIsTargetted(HttpServletRequest httpServletRequest) {
+		
+		String[] virtualHosts = this._redirectFilterConfiguration.redirectFilterTargetVirtualHosts();
+		for(int i = 0; i < virtualHosts.length; i++) {
+			String virtualHost = virtualHosts[i];
+			if(httpServletRequest.getServerName().equals(virtualHost)) return true;
+		}
+		
+		return false;
+	}
+
+	private boolean hasCookie(String cookieName, HttpServletRequest httpServletRequest) {
+		
+		Cookie[] cookies = httpServletRequest.getCookies();
+		for(int i = 0; i < cookies.length; i++) {
+			Cookie cookie = cookies[i];
+			if(cookie.getName().equals(cookieName)) return true;
+		}
+
+		LOG.debug("Does not have cookie {}", cookieName);
+
+		return false;
+		
+	}
+	
+	private volatile RedirectFilterConfiguration _redirectFilterConfiguration;
+	
+	@Activate
+	@Modified
+	private void activate(Map<String, String> properties) {
+
+		this._redirectFilterConfiguration = ConfigurableUtil.createConfigurable(RedirectFilterConfiguration.class, properties);
+		
+	}
+}
---
+++ modules/redirect-filter/src/main/java/com/liferay/samples/fbo/redirect/filter/configuration/RedirectFilterConfiguration.java
@@ -0,0 +1,28 @@
+package com.liferay.samples.fbo.redirect.filter.configuration;
+
+import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClassDefinition;
+
+import aQute.bnd.annotation.metatype.Meta;
+
+@ExtendedObjectClassDefinition(
+		category = "third-party", scope = ExtendedObjectClassDefinition.Scope.SYSTEM
+	)
+@Meta.OCD(
+	    id = "com.liferay.samples.fbo.redirect.filter.configuration.RedirectFilterConfiguration",
+	    localization = "content/Language", name = "redirect-filter-configuration-name"
+	)
+public interface RedirectFilterConfiguration {
+
+	@Meta.AD(deflt = "false", name = "redirect-filter-enabled", required = false)
+	public boolean enabled();
+	
+	@Meta.AD(deflt = "", name = "redirect-trigger-cookie-name", required = false)
+	public String redirectTriggerCookieName();
+
+	@Meta.AD(deflt = "", name = "redirect-filter-url", required = false)
+	public String redirectFilterUrl();
+
+	@Meta.AD(deflt = "", name = "redirect-filter-target-virtual-hosts", required = false)
+	public String[] redirectFilterTargetVirtualHosts();
+	
+}
